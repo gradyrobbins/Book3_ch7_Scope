2a5f51b51c974bafb1b13d7c4008212eca8007ee
Grady Robbins <gradyrobbins13@gmail.com>
2018-12-28 10:41:54 -0600

    c is for cookie complete

--- main.js
+++ main.js
@@ -9,6 +9,16 @@ const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "
 //     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
 // }
 
+// let display = document.getElementById("display-container");
 
+
+for (let y = 0; y < cookies.length; y++) {
+    let display = document.getElementById("display-container");
+    const currentCookie = cookies[y]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+    display.innerHTML += `Mmmmmmm... that's a good ${currentCookie} cookie`
+    display.innerHTML += '<br></br>'
+
+}
 
 
