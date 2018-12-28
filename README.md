acaf4b4b5f81c88d9555d2f834195a2148c92151
Grady Robbins <gradyrobbins13@gmail.com>
2018-12-28 10:32:55 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Book 3 Ch 7/Scope
+## Scope Exercises
+
+### Some software developers were contracted to build some code, but there weren't quite clear yet on how scope works. Your job is to fix their code to stop all the errors from being thrown.
+
+#### C is for Cookie
+#### Conjunction Function
+#### Mod Squad
+#### Simon Says
---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("hello world main.js");
+console.log("C is for Cookie");
+
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+// let y = 1
+
+// for (let y = 0; y < cookies.length; y++) {
+//     const currentCookie = cookies[y]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// }
+
+
+
+
